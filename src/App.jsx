7274d1bda3c03dea4375a4b2c8dfa6dd7c2ef9ab
ratulislam46 <arfanratul46@gmail.com>
2025-11-18 16:01:34 +0600

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import From from './Components/From'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-center mt-10 text-3xl font-semibold'>6Sense job task</h2>
+      <From/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/From.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const From = () => {
+    return (
+        <div>
+            <h1>Form</h1>
+        </div>
+    );
+};
+
+export default From;
